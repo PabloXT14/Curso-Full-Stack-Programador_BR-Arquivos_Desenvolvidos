1c95e81a00aa0027cbba64221ce1bd5766788389
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-01-29 19:08:47 -0300

    new alteration

---
+++ 04 - HTML Intermediário/testeJavascript.js
@@ -0,0 +1,12 @@
+let name = prompt('Digite seu nome!');
+
+let numeros = prompt('Digite um número qualquer entre 0 e 100');
+
+ if((numeros >= 0) && (numeros =< 100)){
+    document.write();
+ }
+ else{
+     alert('O valor inserido não pertence ao intervalo de números');
+ }
+
+// alert('Seja Bem-Vindo '+ name +'!');
