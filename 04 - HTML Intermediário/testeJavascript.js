154d2894748fd2bd7cdc0b9d43c2c23b2fe9362e
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-01-29 19:12:07 -0300

    new alteration

--- 04 - HTML Intermediário/testeJavascript.js
+++ 04 - HTML Intermediário/testeJavascript.js
@@ -2,8 +2,11 @@ let name = prompt('Digite seu nome!');
 
 let numeros = prompt('Digite um número qualquer entre 0 e 100');
 
- if((numeros >= 0) && (numeros =< 100)){
-    document.write();
+ if((numeros >= 0) && (numeros <= 100)){
+     for(let i = 0; i <= numeros; i++){
+        document.write('número: '+ i + '<br>');
+     }
+    
  }
  else{
      alert('O valor inserido não pertence ao intervalo de números');
